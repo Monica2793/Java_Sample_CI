25bdc72e8735fc6f65d5d5e3b6d901c6f87af416
Monica Reddy Tirupari <mrt374@nyu.edu>
2018-06-29 07:58:56 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     // Prints "Hello World" in the treminal window.
     System.out.println("Hello, World!!");
     System.out.println("Trying to trigger a build automatically");
+    System.out.println("checkick the webhook service on jenkins with git");
    }
  }
