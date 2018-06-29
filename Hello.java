9a968f1dd1cb3a68ab606895b9a46dd28a0d5328
Monica Reddy Tirupari <mrt374@nyu.edu>
2018-06-29 07:49:43 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
   public static void main(String[] args) {
     // Prints "Hello World" in the treminal window.
     System.out.println("Hello, World!!");
+    System.out.println("Trying to trigger a buils automatically");
    }
  }
